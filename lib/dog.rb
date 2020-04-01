52b8bbae42575920920c9d63a74b03bcb531a164
Yvonne M Pham <yvonnersz@gmail.com>
2020-04-01 13:23:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,6 @@
 # Your code goes here!
 
-def Dog
+def Dog(dog_name)
   def name=(dog_name)
     @this_dogs_name = dog_name
   end
@@ -13,3 +13,6 @@ def Dog
     puts "woof!"
   end
 end
+
+fido=Dog.new
+fido
